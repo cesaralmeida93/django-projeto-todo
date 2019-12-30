3aa76756cfe9350f39b021cbf89c3f5e6f899031
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2019-12-30 01:00:09 -0300

    first commit

---
+++ .venvtodo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cesar/Documents/cursos/django-projeto-todo/.venvtodo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto todo
+
+Projeto em Django, criação de uma lista de tarefas
