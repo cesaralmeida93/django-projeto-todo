1be13c4ff7197220055777392ec771d4c5bdbb44
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-01-08 10:54:06 -0300

    adicionado login e logout

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
@@ -6,6 +7,7 @@ from .forms import TaskForm
 from .models import Task
 
 
+@login_required
 def taskList(request):
     search = request.GET.get('search')
 
@@ -26,11 +28,13 @@ def taskList(request):
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
 
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -46,6 +50,7 @@ def newTask(request):
         return render(request, 'tasks/addtask.html', {'form': form})
 
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -62,11 +67,13 @@ def editTask(request, id):
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
 
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
     messages.info(request, 'Tarefa deletada com sucesso.')
     return redirect('/')
+
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
